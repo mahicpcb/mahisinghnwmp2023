5e19addfa1b57ca647ebe032acdd54a46d818737
mahicpcb <ms05735@gmail.com>
2024-07-02 10:16:29 +0530

    Update mahisinghnwmpdata2023.py

--- mahisinghnwmpdata2023.py
+++ mahisinghnwmpdata2023.py
@@ -1,21 +1,34 @@
 import pandas as pd
 import streamlit as st
 import altair as alt
+import matplotlib.pyplot as plt
 
-# Load the main data
-df = pd.read_excel('2023 Data.xlsx')
+# Function to load data
+@st.cache
+def load_data(file_path):
+    return pd.read_excel(file_path)
 
+background_color = """
+<style>
+body {
+    background-color: #f0f0f0; /* Replace with your desired background color */
+}
+</style>
+"""
+
+# Display the custom CSS
+st.markdown(background_color, unsafe_allow_html=True)
 # Title and Header
-st.title('2023 NWMP Data')
+st.title('NWMP Data-2023')
 st.header('State-wise Data')
 
-# Dropdown for selecting state-wise data
+# Dropdown for selecting state-wise data 
 file_name = st.selectbox(label='Choose State Data', options=[
     'ANDHRA PRADESH.xlsx', 'ARUNACHAL PRADESH.xlsx', 'ASSAM.xlsx', 'BIHAR.xlsx',
     'CHHATTISGARH.xlsx', 'DAMAN AND DIU, DADRA AND NAGAR HAVELI.xlsx', 'DELHI.xlsx',
     'GOA.xlsx', 'GUJARAT.xlsx', 'HARYANA.xlsx', 'HIMACHAL PRADESH.xlsx',
     'JAMMU & KASHMIR.xlsx', 'JHARKHAND.xlsx', 'KARNATAKA.xlsx', 'KERALA.xlsx',
-    'LAKSHADWEP.xlsx', 'MADHYA PRADESH.xlsx', 'MAHARASHTRA.xlsx', 'MANIPUR.xlsx',
+    'LAKSHADWEEP.xlsx', 'MADHYA PRADESH.xlsx', 'MAHARASHTRA.xlsx', 'MANIPUR.xlsx',
     'MEGHALAYA.xlsx', 'MIZORAM.xlsx', 'NAGALAND.xlsx', 'ODISHA.xlsx',
     'PUDUCHERRY.xlsx', 'PUNJAB.xlsx', 'RAJASTHAN.xlsx', 'SIKKIM.xlsx',
     'TAMIL NADU.xlsx', 'TELANGANA.xlsx', 'TRIPURA.xlsx', 'UTTAR PRADESH.xlsx',
@@ -195,6 +208,73 @@ with col10:
     monitoring_location_max_Ar = water_body_by_data.loc[water_body_by_data['Max Arsenic (mg/L)'] == Maximum_Arsenic, 'Monitoring Location'].iloc[0]
     st.write("Location:",monitoring_location_max_Ar)
 
+st.header("Minimun & Maximum BOD (mg/L) Over Monitoring Location")
+max_bod_chart = alt.Chart(water_body_by_data).mark_line(color='Red').encode(
+    x='Monitoring Location',
+    y='Max BOD (mg/L)',
+    tooltip=['Monitoring Location', 'Max BOD (mg/L)']
+).properties(
+    width=600,
+    height=400
+)
+
+min_bod_chart = alt.Chart(water_body_by_data).mark_line(color='Green').encode(
+    x='Monitoring Location',
+    y='Min BOD (mg/L)',
+    tooltip=['Monitoring Location', 'Min BOD (mg/L)']
+).properties(
+    width=600,
+    height=400
+)
+
+# Create a line for the standard line
+standard_line = alt.Chart(pd.DataFrame({'Standard_Line': [3]})).mark_rule(color='Blue').encode(
+    y='Standard_Line:Q'
+)
+
+# Combine the charts
+chart_with_labels = (max_bod_chart + min_bod_chart + standard_line)
+
+# Add data labels for Max BOD
+max_text = max_bod_chart.mark_text(
+    align='left',
+    baseline='middle',
+    dx=5,
+    dy=-5,
+    color='blue'
+).encode(
+    text='Max BOD (mg/L):Q'
+)
+
+# Add data labels for Min BOD
+min_text = min_bod_chart.mark_text(
+    align='left',
+    baseline='middle',
+    dx=5,
+    dy=15,
+    color='orange'
+).encode(
+    text='Min BOD (mg/L):Q'
+)
+
+# Add data labels for Standard Line
+standard_text = standard_line.mark_text(
+    align='left',
+    baseline='middle',
+    dx=5,
+    dy=-25,
+    color='green'
+).encode(
+    text=alt.value('3')
+)
+
+# Combine the line charts and the data labels
+chart_with_labels = (chart_with_labels + max_text + min_text + standard_text)
+
+# Render the chart using Streamlit
+st.altair_chart(chart_with_labels, use_container_width=True)
+
+
 
 st.header('River Wise Data')
 
@@ -286,7 +366,7 @@ with col10:
 st.header("Minimun & Maximum BOD (mg/L) Over Monitoring Location")
 # st.line_chart(River_by_data.set_index('Monitoring Location')[['Max BOD (mg/L)','Min BOD (mg/L)']].assign(Standard_Line=3))
 
-max_bod_chart = alt.Chart(River_by_data).mark_line(color='blue').encode(
+max_bod_chart = alt.Chart(River_by_data).mark_line(color='red').encode(
     x='Monitoring Location',
     y='Max BOD (mg/L)',
     tooltip=['Monitoring Location', 'Max BOD (mg/L)']
@@ -295,7 +375,7 @@ max_bod_chart = alt.Chart(River_by_data).mark_line(color='blue').encode(
     height=400
 )
 
-min_bod_chart = alt.Chart(River_by_data).mark_line(color='orange').encode(
+min_bod_chart = alt.Chart(River_by_data).mark_line(color='Green').encode(
     x='Monitoring Location',
     y='Min BOD (mg/L)',
     tooltip=['Monitoring Location', 'Min BOD (mg/L)']
@@ -305,7 +385,7 @@ min_bod_chart = alt.Chart(River_by_data).mark_line(color='orange').encode(
 )
 
 # Create a line for the standard line
-standard_line = alt.Chart(pd.DataFrame({'Standard_Line': [3]})).mark_rule(color='green').encode(
+standard_line = alt.Chart(pd.DataFrame({'Standard_Line': [3]})).mark_rule(color='Blue').encode(
     y='Standard_Line:Q'
 )
 
@@ -350,3 +430,38 @@ chart_with_labels = (chart_with_labels + max_text + min_text + standard_text)
 
 # Render the chart using Streamlit
 st.altair_chart(chart_with_labels, use_container_width=True)
+
+st.header('NWMP Wetland / Ramsar Locations ')
+@st.cache_resource
+def load_data(file_path):
+    return pd.read_excel(file_path)
+
+def main():
+    # Button to trigger data loading and display
+    if st.button('126 Ramsar Sites Locations on 76 Weland/Ramsar'):
+        file_name = 'NWMP Wetland List.xlsx'  # Assuming fixed file name/path
+        Wetland_Ramsar_data = load_data(file_name)
+        st.dataframe(Wetland_Ramsar_data)
+
+if __name__ == '__main__':
+    main()
+
+
+
+st.header('Medium & Minor River Data-2023')
+@st.cache_resource
+def load_data(file_path):
+    return pd.read_excel(file_path)
+
+def main():
+    # Button to trigger data loading and display
+    if st.button('Load Data'):
+        file_name = 'Medium & Minor River data -2023.xlsx'  # Assuming fixed file name/path
+        Medium_And_Minor_River = load_data(file_name)
+        st.dataframe(Medium_And_Minor_River)
+
+
+if __name__ == '__main__':
+    main()
+
+
